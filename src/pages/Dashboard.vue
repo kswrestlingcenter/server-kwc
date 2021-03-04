d965fb15402334fb14324fedba4331ab0767ec9c
Ambra <ambra.isenberg@gmail.com>
2021-03-03 21:00:58 -0600

    added contact form functionality

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -67,6 +67,39 @@
         </tbody>
       </template>
     </v-simple-table>
+    <br>
+    <h2>Contact: </h2>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Email
+            </th>
+            <th class="text-left">
+              subject
+            </th>
+            <th class="text-left">
+              created
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="contact in contactFormResponses"
+            :key="contact.id"
+          >
+            <td>{{ contact.firstName }}</td>
+            <td>{{ contact.email }}</td>
+            <td>{{ contact.subject }}</td>
+            <td>{{ new Date(contact.created).toDateString() }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-container>
 </template>
 
@@ -76,14 +109,20 @@ import axios from 'axios'
 export default {
   name: 'DashboardPage',
   async created() {
-    let {data} = await axios.get('api/mailingList')
+    const { data } = await axios.get('api/mailingList')
+    const { data: contact }= await axios.get('api/contactForm')
     console.log("marketingSubscribers:", JSON.stringify(data.subscribers, null, 2))
     this.marketingSubscribers = data.subscribers
+    console.log("contactFORMresp:", contact.contactResponses)
+    this.contactFormResponses = contact.contactResponses
   },
   data() {
     return {
       marketingSubscribers: [ // Mock Data
         {id: 1, name: "ERROR: Data did not load from DB", email: "mockData@email.com", mailingList: true, created: new Date()}
+      ],
+      contactFormResponses: [ // Mock Data
+        {id: 1, firstName: "ERROR: Data did not load from DB", email: "mockData@email.com", created: new Date()}
       ],
       events: [ // Mock Data
         {id: 1, name: "Event 1", date: "today", eventFlyer: "http://eventURL.pdf"},
